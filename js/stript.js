ff4594fe2adb021d39dfec7b81571cf02b23f5c8
Urszula Majcher <ulunka.m@gmail.com>
2024-02-18 18:43:07 +0100

    adding function

--- js/stript.js
+++ js/stript.js
@@ -1,36 +1,52 @@
-console.log("Hejka, jak Ci mija dzień?");
+{
+  const welcome = () => {
+    console.log("Hejka, jak Ci mija dzień?");
+  };
 
-const buttonImage = document.querySelector(".js-buttonFullScr");
-const image = document.querySelector(".section__image");
+  const navigationList = document.querySelector(".js-navigationList");
+  const listThemeName = document.querySelector(".js-listThemeName");
 
-buttonImage.addEventListener("click", () => {
-  image.requestFullscreen();
-});
+  const onClickHideList = () => {
+    navigationList.classList.toggle("navigation__list--hide");
 
-const buttonHideList = document.querySelector(".js-buttonList");
-const navigationList = document.querySelector(".js-navigationList");
-const listThemeName = document.querySelector(".js-listThemeName");
+    listThemeName.innerText = navigationList.classList.contains(
+      "navigation__list--hide"
+    )
+      ? "Pokaż"
+      : "Ukryj";
+  };
 
-buttonHideList.addEventListener("click", () => {
-  navigationList.classList.toggle("navigation__list--hide");
+  const image = document.querySelector(".section__image");
 
-  listThemeName.innerText = navigationList.classList.contains(
-    "navigation__list--hide"
-  )
-    ? "Pokaż"
-    : "Ukryj";
-});
+  const onClickImgFullScreen = () => {
+    image.requestFullscreen();
+  };
 
-const toggleBackgroud = document.querySelector(".js-toggleBackgroud");
-const mainBackground = document.querySelector(".container");
-const backgroundThemeName = document.querySelector(".js-backgroundThemeName");
+  const mainBackground = document.querySelector(".container");
+  const backgroundThemeName = document.querySelector(".js-backgroundThemeName");
 
-toggleBackgroud.addEventListener("click", () => {
-  mainBackground.classList.toggle("container--otherBackground");
+  const onChangeBackgroundClick = () => {
+    mainBackground.classList.toggle("container--otherBackground");
 
-  backgroundThemeName.innerText = mainBackground.classList.contains(
-    "container--otherBackground"
-  )
-    ? "Szare"
-    : "Zielone";
-});
+    backgroundThemeName.innerText = mainBackground.classList.contains(
+      "container--otherBackground"
+    )
+      ? "Szare"
+      : "Zielone";
+  };
+
+  const init = () => {
+    const buttonHideList = document.querySelector(".js-buttonList");
+    buttonHideList.addEventListener("click", onClickHideList);
+
+    const buttonImage = document.querySelector(".js-buttonFullScr");
+    buttonImage.addEventListener("click", onClickImgFullScreen);
+
+    const toggleBackgroud = document.querySelector(".js-toggleBackgroud");
+    toggleBackgroud.addEventListener("click", onChangeBackgroundClick);
+
+    welcome();
+  };
+
+  init();
+}
